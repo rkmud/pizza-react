c1ca4266c31b7e9cd41c9c8b749aab4caa1fbd92
«ustinaa» <ustkorbut@gmail.com>
2024-03-27 00:08:58 +0300

    initial commit

--- src/companents/Loader/Loader.jsx
+++ src/companents/Loader/Loader.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Flex, Spin } from 'antd';
 import styles from './Loader.module.scss'
+import { Flex, Spin} from 'antd';
 export const Loader = () => {
   return (
     <div className={styles.wrapper}>
